57dfd1ecfbbebcbbe3a0923644ee25340ad87e4b
Song <yousonghou@yahoo.com>
2019-03-01 23:28:45 +0000

    add author and category field

--- src/component/deals/AddDeal.js
+++ src/component/deals/AddDeal.js
@@ -11,11 +11,13 @@ class AddDeal extends Component{
             company: '',
             name: '',
             price: '',
+            category: '',
             image: null,
             imageName: 'Choose file',
             address: '',
             city: '',
             description: '',
+            author: '',
             errors: {}
         };
         this.onChange = this.onChange.bind(this);
@@ -42,16 +44,17 @@ class AddDeal extends Component{
     
     onSubmit(e){
         e.preventDefault();
+        
         const formData = new FormData();
         formData.append('company', this.state.company);
         formData.append('price', this.state.price);
         formData.append('name', this.state.name);
-        formData.append('category', "food");
+        formData.append('category', this.state.category);
         formData.append('image', this.state.image);
         formData.append('location', this.state.address);
-        formData.append('city', "brooklyn");
+        formData.append('city', this.state.city);
         formData.append('description', this.state.description);
-        formData.append('author', this.state.name);
+        formData.append('author', this.state.author);
 
         console.log('formData', formData);
 
@@ -110,6 +113,21 @@ class AddDeal extends Component{
                             />
                             {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
                         </div>
+                        
+                        <div className="form-group">
+                            <label htmlFor="text">Category</label>
+                            <input
+                                type="text"
+                                className={classnames('form-control form-control-lg', {
+                                    'is-text': errors.category
+                                })}
+                                name="category"
+                                value={this.state.category}
+                                onChange={this.onChange} 
+                            />
+                            {errors.category && (<div className="invalid-feedback">{errors.category}</div>)}
+                        </div>
+                        
                         <div className="form-group">
                             <label htmlFor="text">Item price</label>
                             <input
@@ -169,11 +187,11 @@ class AddDeal extends Component{
                                     
                                 >
                                     <option value=" ">City</option>
-                                    <option value="Manhattan">Manhattan</option>
-                                    <option value="Queens">Queens</option>
-                                    <option value="Bronx">Bronx</option>
-                                    <option value="Brooklyn">Brooklyn</option>
-                                    <option value="Staten Island">Staten Island</option>
+                                    <option value="manhattan">Manhattan</option>
+                                    <option value="queens">Queens</option>
+                                    <option value="bronx">Bronx</option>
+                                    <option value="brooklyn">Brooklyn</option>
+                                    <option value="staten island">Staten Island</option>
 
                                 </select>
                                 
@@ -200,7 +218,20 @@ class AddDeal extends Component{
                             </textarea>
                         </div>
 
-
+                        <div className="form-group">
+                            <label htmlFor="text">Author</label>
+                            <input
+                                type="text"
+                                className={classnames('form-control form-control-lg', {
+                                    'is-text': errors.author
+                                })}
+                                name="author"
+                                value={this.state.author}
+                                onChange={this.onChange} 
+                            />
+                            {errors.author && (<div className="invalid-feedback">{errors.author}</div>)}
+                        </div>
+                        
                         <input type="submit" className="btn btn-info btn-block mt-4" />
                       </form>
                     </div>
