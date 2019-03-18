0f692ebb054e0b3f81016e9aa7746e9d62fc8738
electrone901 <carbajalluis@rocketmail.com>
2019-03-18 15:40:31 -0400

    remove signup buttom

--- src/component/carousel/TextSlider.js
+++ src/component/carousel/TextSlider.js
@@ -3,24 +3,47 @@ Title           : LeftArrow.js
 Description     : is the left arrow of our carousel  using font-awesome 
 ******************************************************************************/
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-// import '../../sass/main.scss';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-// functional component
-const TextSlider = () => {
-    return (
-        <div className="tParent">
-            <div className="row">
-                <div className="col text-center">
-                    <h1 className="">Find the best NY deals</h1>
-                    <p className="tParent__items__subTittle">Do more with less</p>
-                    <Link to="/register" className="tParent__items__sign">Sign Up</Link>
+class TextSlider extends Component {
+    constructor() {
+        super();
+        this.state = {
+            auth: false
+        }
+    }
+    componentDidMount() {
+        if(this.props.auth.isAuthenticated) {
+            console.log(' YES user logged this.props.auth.isAuthenticated', this.props)
+            this.setState({
+                auth: true
+            })
+        }
+    }
+    render() {
+        return (
+            <div className="tParent">
+                <div className="row">
+                    <div className="col text-center">
+                        <h1 className="">Find the best NY deals</h1>
+                        <p className="tParent__items__subTittle">Do more with less</p>
+                        {
+                            this.state.auth ? null:<Link to="/register" className="tParent__items__sign">Sign Up</Link>
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-export default TextSlider;
 
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps) (withRouter(TextSlider));
+  
 
