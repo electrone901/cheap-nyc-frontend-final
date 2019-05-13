97309cacaf0fd1add63c654aa6a5d4289e494786
Song <yousonghou@yahoo.com>
2019-05-13 22:38:29 +0000

    use flex on review popup

--- src/component/review/ReviewPopup.js
+++ src/component/review/ReviewPopup.js
@@ -10,15 +10,15 @@ class ReviewPopup extends Component {
           <div className='popup_inner'>
             <h1 className="space-top">{this.props.title}</h1>
             <p className="small">{this.props.text}</p>
-            <div className="row text-center">
-              <div className="col-lg-6 col-md-6 col-sm-4 padding-top">
+            <div className="d-flex justify-content-around flex-wrap">
+              <div className="padding-top my-1">
                 <Link to={`/addReview/${id}`} className="btn btn-primary btn-width" onClick={this.props.closePopup}>As Member <i className="fa fa-check" aria-hidden="true"></i></Link>
               </div>
-              <div className="col-lg-6 col-md-6 col-sm-4 padding-top">
+              <div className="padding-top my-1">
                 <Link to={`/addReview-guest/${id}`} className="btn btn-primary btn-width" onClick={this.props.closePopup}>As Guest</Link>
               </div>
             </div>
-            <div className="padding-top">
+            <div className="padding-top mt-3">
               <button className="btn-info-helpful" onClick={this.props.closePopup}>Close me</button>
             </div>
           </div>
