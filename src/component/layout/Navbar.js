4ec2d1294cf634fd1367bddd3371242ae47290a5
You Song Hou <yousonghou@yahoo.com>
2019-10-27 21:21:15 -0400

    show active link on the navbar

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import logo from '../../img/l3.png';
+import classnames from 'classnames';
 
+import logo from '../../img/l3.png';
 import { logoutUser } from '../../actions/authActions';
 
 class Navbar extends Component {
   constructor() {
     super();
     this.state = {
-      showPopup: ''
+      showPopup: '',
+      currentLink: ''
     };
-  }
+  };
 
   onLogoutClick(e){
       e.preventDefault();
+      this.onChangeLink("");
       this.props.logoutUser(this.props.history);
-  }
+  };
+
+  onChangeLink(name){
+    this.setState({ currentLink: name });
+  };
 
   profile() {
     const { user } = this.props.auth;
     this.props.history.push(`/profile/${user.id}`);
-  }
-
+  };
   
   render() {
     const {isAuthenticated, user} = this.props.auth;
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-          <Link to={`/profile/${user.id}`} className="nav-link">
+          <Link
+            to={`/profile/${user.id}`}
+            className={classnames("nav-link", 
+              this.state.currentLink === 'profile' ? 'active' : null
+            )}
+            onClick={() => this.onChangeLink("profile")}>
             Profile
           </Link>
         </li>
@@ -45,12 +56,22 @@ class Navbar extends Component {
     const guestLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-          <Link className="nav-link" to="/register">
+          <Link
+            to="/register"
+            className={classnames("nav-link", 
+              this.state.currentLink === 'register' ? 'active' : null
+            )}
+            onClick={() => this.onChangeLink("register")}>
             Sign Up
           </Link>
         </li>
         <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-          <Link className="nav-link" to="/login">
+          <Link
+            to="/login"
+            className={classnames("nav-link", 
+              this.state.currentLink === 'login' ? 'active' : null
+            )}
+            onClick={() => this.onChangeLink("login")}>
             Login
           </Link>
         </li>
@@ -60,7 +81,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-sm navbar-light btn-light sticky-top">
         <div className="container">
-          <Link className="navbar-brand logo" to="/">
+          <Link className="navbar-brand logo" to="/" onClick={() => this.onChangeLink("")}>
             <img src={logo} alt="logo"/>
           </Link>
 
@@ -84,22 +105,42 @@ class Navbar extends Component {
             <ul className="navbar-nav text-right">
 
               <li className="nav-item text-left" data-toggle="collapse" data-target=".navbar-collapse.show">
-                <Link className="nav-link" to="/howItWorks">
+                <Link
+                  to="/howItWorks"
+                  className={classnames("nav-link", 
+                    this.state.currentLink === 'howItWorks' ? 'active' : null
+                  )}
+                  onClick={() => this.onChangeLink("howItWorks")}>
                   How it works
                 </Link>
               </li>
               <li className="nav-item text-left" data-toggle="collapse" data-target=".navbar-collapse.show">
-                <Link className="nav-link" to="/membership">
+                <Link
+                  to="/membership"
+                  className={classnames("nav-link", 
+                    this.state.currentLink === 'membership' ? 'active' : null
+                  )}
+                  onClick={() => this.onChangeLink("membership")}>
                   Membership 
                 </Link>
               </li>
               <li className="nav-item text-left" data-toggle="collapse" data-target=".navbar-collapse.show">
-                <Link className="nav-link" to="/feedback">
+                <Link
+                  to="/feedback"
+                  className={classnames("nav-link", 
+                    this.state.currentLink === 'feedback' ? 'active' : null
+                  )}
+                  onClick={() => this.onChangeLink("feedback")}>
                   Feedback 
                 </Link>
               </li>
               <li className="nav-item text-left" data-toggle="collapse" data-target=".navbar-collapse.show">
-                <Link className="nav-link" to="/contact-us">
+                <Link
+                  to="/contact-us"
+                  className={classnames("nav-link", 
+                    this.state.currentLink === 'contact-us' ? 'active' : null
+                  )}
+                  onClick={() => this.onChangeLink("contact-us")}>
                   Contact Us
                 </Link>
               </li>
