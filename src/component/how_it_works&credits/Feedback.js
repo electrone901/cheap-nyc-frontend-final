8e14aba6ab67c6603b0417dbdd6f5368f228ba41
You Song Hou <yousonghou@yahoo.com>
2019-11-12 01:17:17 -0500

    fix warning message

--- src/component/how_it_works&credits/Feedback.js
+++ src/component/how_it_works&credits/Feedback.js
@@ -4,12 +4,20 @@ class Feedback extends Component{
   componentDidMount() {
     window.scrollTo(0,0);
   }
+
   render(){
-      
       return(
           <div className="text-center">
-            <iframe className="frame-custome" title="feedback" src="https://docs.google.com/forms/d/e/1FAIpQLSeMpj-bBwnr7MJ0ba8ExaWFpwRlShtkVx9Mw8W6RSN9zWXssw/viewform?embedded=true"  frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-
+            <iframe
+              className="frame-custome"
+              title="feedback"
+              src="https://docs.google.com/forms/d/e/1FAIpQLSeMpj-bBwnr7MJ0ba8ExaWFpwRlShtkVx9Mw8W6RSN9zWXssw/viewform?embedded=true"
+              frameBorder="0"
+              marginHeight="0"
+              marginWidth="0"
+              >
+                Loading...
+            </iframe>
           </div>
       );
   }
--- src/component/how_it_works&credits/Membership.js
+++ src/component/how_it_works&credits/Membership.js
@@ -16,47 +16,47 @@ class Contact extends Component{
                 <div className="row -row-social">
                   <div className="col-lg-4 col-md-6 col-sm-12 text-center">
                     <hr className="bg-danger mb-4 py-1" />
-                    <div class="card mb-3">
-                      <div class="card-body">
-                        <p class="btn btn-primary btn-block">Basic</p>
-                        <p class="card-text my-0">1 location</p>
-                        <p class="card-text">2 posting</p>
-                        <h3 class="card-title text-info">9.99/month</h3>
-                        <h5 class="card-title text-danger">2 months free</h5>
-                        <p class="card-text">(single menu item)</p>
-                        <p class="card-text mb-4">Basic analytics</p>
+                    <div className="card mb-3">
+                      <div className="card-body">
+                        <p className="btn btn-primary btn-block">Basic</p>
+                        <p className="card-text my-0">1 location</p>
+                        <p className="card-text">2 posting</p>
+                        <h3 className="card-title text-info">9.99/month</h3>
+                        <h5 className="card-title text-danger">2 months free</h5>
+                        <p className="card-text">(single menu item)</p>
+                        <p className="card-text mb-4">Basic analytics</p>
                       </div>
                     </div>
                     <hr className="bg-danger pb-1" />
                   </div>
                   <div className="col-lg-4 col-md-6 col-sm-12 text-center">
                     <hr className="bg-danger mb-4 py-1" />
-                    <div class="card mb-3">
-                      <div class="card-body">
-                        <p class="btn btn-primary btn-block">Starter</p>
-                        <p class="card-text my-0">Up to 3 locations</p>
-                        <p class="card-text">5 posting each</p>
-                        <h3 class="card-title text-info">15.99/month</h3>
-                        <h5 class="card-title text-danger">2 months free</h5>
-                        <p class="card-text">(single menu item)</p>
-                        <p class="card-text my-0">Basic analytics</p>
-                        <p class="card-text">Tracking code integration</p>
+                    <div className="card mb-3">
+                      <div className="card-body">
+                        <p className="btn btn-primary btn-block">Starter</p>
+                        <p className="card-text my-0">Up to 3 locations</p>
+                        <p className="card-text">5 posting each</p>
+                        <h3 className="card-title text-info">15.99/month</h3>
+                        <h5 className="card-title text-danger">2 months free</h5>
+                        <p className="card-text">(single menu item)</p>
+                        <p className="card-text my-0">Basic analytics</p>
+                        <p className="card-text">Tracking code integration</p>
                       </div>
                     </div>
                     <hr className="bg-danger mb-4 pb-1" />
                   </div>
                   <div className="col-lg-4 col-md-6 col-sm-12 text-center">
                     <hr className="bg-danger mb-4 py-1" />
-                    <div class="card mb-3">
-                      <div class="card-body">
-                        <p class="btn btn-primary btn-block">Expert</p>
-                        <p class="card-text my-0">Up to 20 locations</p>
-                        <p class="card-text">20 posting each</p>
-                        <h3 class="card-title text-info">20.99/month</h3>
-                        <h5 class="card-title text-danger">2 months free</h5>
-                        <p class="card-text">(single menu item)</p>
-                        <p class="card-text my-0">Advanced analytics</p>
-                        <p class="card-text">Tracking code integration</p>
+                    <div className="card mb-3">
+                      <div className="card-body">
+                        <p className="btn btn-primary btn-block">Expert</p>
+                        <p className="card-text my-0">Up to 20 locations</p>
+                        <p className="card-text">20 posting each</p>
+                        <h3 className="card-title text-info">20.99/month</h3>
+                        <h5 className="card-title text-danger">2 months free</h5>
+                        <p className="card-text">(single menu item)</p>
+                        <p className="card-text my-0">Advanced analytics</p>
+                        <p className="card-text">Tracking code integration</p>
                       </div>
                     </div>
                     <hr className="bg-danger mb-4 pb-1" />
