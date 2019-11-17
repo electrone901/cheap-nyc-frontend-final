4ded230132e570ebda2ae4e3eacb108da9737da7
You Song Hou <yousonghou@yahoo.com>
2019-11-17 01:13:57 -0500

    break up index.css into sections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/auth.css';
+import './css/carousel.css';
+import './css/deal.css';
+import './css/profile.css';
+import './css/review.css';
+import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
