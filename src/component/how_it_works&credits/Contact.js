50bc0b50bf690b1d0c94d35443066eb4a0aee8cb
You Song Hou <yousonghou@yahoo.com>
2020-04-21 00:22:30 -0400

    refactor contact page

--- src/component/how_it_works&credits/Contact.js
+++ src/component/how_it_works&credits/Contact.js
@@ -9,6 +9,7 @@ import ReactGA from 'react-ga';
 export const initGA = () => {
   ReactGA.initialize('UA-142224072-1');
 }
+
 export const loadPageView = () => {
   ReactGA.set({page: window.location.pathname});
   ReactGA.pageview(window.location.pathname);
@@ -20,34 +21,39 @@ class Contact extends Component{
     initGA();
     loadPageView();
   }
+
   render(){
-      
-      return(
-          <div className="container">
-            <br/>
-            <div className="text-center">
-              <div className="jumbotron">
-                <h1>Contact Us</h1>
-                <h2>Email: realcheapny@gmail.com</h2>
-                <h2>Check us out on our social media!</h2><br/>
-                <div className="row -row-social">
-                  <div className="col-lg-3 col-md-3 col-sm-6 text-center">
-                    <a href="https://www.facebook.com/realcheapny/" target="blank"><img className="social-thumbnail" alt="facebookIcon" src={fb}/></a>
-                  </div>
-                  <div className="col-lg-3 col-md-3 col-sm-6 text-center">
-                    <a href="https://www.instagram.com/realcheapny/" target="blank"><img className="social-thumbnail" alt="instagramIcon" src={instagram}/></a>
-                  </div>
-                  <div className="col-lg-3 col-md-3 col-sm-6 text-center">
-                    <a href="https://twitter.com/realcheapny" target="blank"><img className="social-thumbnail" alt="twitterIcon" src={twitter}/></a>
-                  </div>
-                  <div className="col-lg-3 col-md-3 col-sm-6 text-center">
-                    <a href="mailto:realcheapny@gmail.com"><img className="social-thumbnail" alt="emailIcon" src={email}/></a>
-                  </div>
-                </div>
-              </div>
+    return(
+      <div className="container mt-5 text-center">
+        <div className="jumbotron">
+          <h1 className="color-p">Contact Us</h1>
+          <h3 className="mb-5">Email: realcheapny@gmail.com</h3>
+          <h2 className="mb-4">Check us out on our social media!</h2>
+          <div className="d-flex justify-content-around flex-wrap">
+            <div className="mb-3">
+              <a href="https://www.facebook.com/realcheapny/" target="blank">
+                <img className="social-thumbnail" alt="facebookIcon" src={fb}/>
+              </a>
+            </div>
+            <div className="mb-3">
+              <a href="https://www.instagram.com/realcheapny/" target="blank">
+                <img className="social-thumbnail" alt="instagramIcon" src={instagram}/>
+              </a>
+            </div>
+            <div className="mb-3">
+              <a href="https://twitter.com/realcheapny" target="blank">
+                <img className="social-thumbnail" alt="twitterIcon" src={twitter}/>
+              </a>
+            </div>
+            <div className="mb-3">
+              <a href="mailto:realcheapny@gmail.com">
+                <img className="social-thumbnail" alt="emailIcon" src={email}/>
+              </a>
             </div>
           </div>
-      );
+        </div>
+      </div>
+    );
   }
 }
 export default Contact;
