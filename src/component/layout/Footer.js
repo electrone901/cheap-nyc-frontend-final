7e6c7161f63d62da52db22aa6065467e75cdcd4b
You Song Hou <yousonghou@yahoo.com>
2020-07-01 00:44:53 -0400

    change the background color of the footer

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 
 export default() => {
     return (
-        <footer className="bg-dark text-white mt-5 p-4 text-center">
+        <footer className="bg-danger text-white mt-5 p-4 text-center">
 
             <section className="footer">
                 
